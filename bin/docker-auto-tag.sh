ca791389cd96d01fcab3817575dbdce2d18d5eb3
Riccardo Carlesso <palladiusbonton@gmail.com>
2025-05-26 16:31:44 +0200

    1.10 added another script, still WIP

---
+++ bin/docker-auto-tag.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+IMAGE_NAME="$1"
+#set -euo pipefail
+
+echo "Starting Docker Auto Tag Script... TODO"
+echo "This script is a placeholder and does not perform any actions yet."
+echo " - IMAGE_NAME: $IMAGE_NAME"
+echo " - PROJECT_ID: $PROJECT_ID"
+echo " - VERSION: $(cat VERSION)"
+
